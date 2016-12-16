fa8470029c0e856917f7695d2f3358d10cbaf009
Manuel Sammer <games.games1@gmx.at>
2016-12-16 12:50:44 +0100

    Fixed Fix 204

--- routes/rating.js
+++ routes/rating.js
@@ -66,20 +66,24 @@ router.get('/ratings/:_id', function (req, res, next) {
                 next(err)
             }
         });
-        request.on('row', function (columns) {
+         request.on('row', function (columns) {
             columns.forEach(function (column) {
                 if (column.value === null) {
                     result.push('NULL');
                 } else {
                     result.push(column.value);
                 }
+               
             });
-            console.log(result);
-            if (!result) {
-                next(err)
-            }
+                ratings.push(new Rating(result[0], result[1], new Date(result[2]), result[3], result[4]));
+                console.log(result);
+                result = [];
+            });
+           request.on('doneInProc', function (rowCount, more, rows) {
+            console.log(rowCount + ' rows returned');
             connection.release();
             res.type('application/json');
+            var ret = [];
             for(var i=0;i<ratings.length;i++) {
                 var d = new Date(ratings[i].Date);
                 ret.push({"Grade": ratings[i].Grade, "Feedback":ratings[i].Feedback, "Date":  d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate() , "IdUserAccount":ratings[i].IdUserAccount, "IdLocation":ratings[i].IdLocation});
@@ -91,9 +95,10 @@ router.get('/ratings/:_id', function (req, res, next) {
                 res.send(ret);
             }
         });
-        connection.execSql(request);
+         connection.execSql(request);
     });
 });
+
 
 router.post('/ratings', function (req, res, next) {
     pool.acquire(function (err, connection) {
@@ -120,50 +125,7 @@ router.post('/ratings', function (req, res, next) {
             res.send({message: 'Rating successfully added'});
         });
         connection.execSql(request);
-    });
 });
-/*
-router.delete('/ratings/:_id', function (req, res, next) {
-    pool.acquire(function (err, connection) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        request = new Request("DELETE FROM Spoty.Rating WHERE IdCity =" + req.params._id + ";", function (err) {
-            if (err) {
-                next(err)
-            }
-        });
-        request.on('doneInProc', function (columns) {
-            connection.release();
-            res.send({message: 'Rating successfully deleted'});
-        });
-        connection.execSql(request);
-    });
 });
 
-router.put('/ratings/:_id', function (req, res, next) {
-    pool.acquire(function (err, connection) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        var city = req.body;
-        request = new Request("UPDATE Spoty.Rating SET PostalCode = @PostalCode, CityName = @CityName, IdCounty = @IdCounty WHERE IdCity= @IdCity;", function (err) {
-            if (err) {
-                next(err)
-            }
-        });
-        request.addParameter('IdCity', types.Int,  city.IdCity);
-        request.addParameter('PostalCode', types.Int,city.PostalCode);
-        request.addParameter('CityName', types.NVarChar,  city.CityName);
-        request.addParameter('IdCounty', types.Int,  city.IdCounty);
-        request.on('doneInProc', function (columns) {
-            connection.release();
-            res.send({message: 'Rating successfully updated'});
-        });
-        connection.execSql(request);
-    });
-});
-*/
 module.exports = router;
