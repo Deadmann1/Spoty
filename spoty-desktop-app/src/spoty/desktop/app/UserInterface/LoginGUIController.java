bee51b250470ba2f43ea60f4a13b2a760b4ef3ca
lampr <lampr@Daniel>
2017-02-02 16:07:35 +0100

    TdoT Release V2

--- spoty-desktop-app/src/spoty/desktop/app/UserInterface/LoginGUIController.java
+++ spoty-desktop-app/src/spoty/desktop/app/UserInterface/LoginGUIController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import spoty.desktop.app.Database.UserAccountDatabase;
 import javafx.scene.layout.AnchorPane;
@@ -44,6 +46,9 @@ public class LoginGUIController {
 
     @FXML
     private TextField txtUsername;
+    
+    @FXML
+    private ImageView imageViewLogo;
 
 
     @FXML
--- spoty-desktop-app/src/spoty/desktop/app/UserInterface/MainGUIController.java
+++ spoty-desktop-app/src/spoty/desktop/app/UserInterface/MainGUIController.java
@@ -17,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.AnchorPane;
@@ -86,6 +87,9 @@ public class MainGUIController {
     
     @FXML
     private Label lblInfoMessageMainMenu;
+    
+    @FXML
+    private ImageView imageViewLogo;
 
 
     @FXML
